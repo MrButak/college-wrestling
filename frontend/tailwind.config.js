c4180d20b397d3b7a12f72ae508b3c9bcde67347
Matthew Spence <mspence5555@gmail.com>
2022-04-15 14:51:26 -0500

    ...

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,12 +2,5 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    './node_modules/tw-elements/dist/js/**/*.js'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tw-elements/dist/plugin')
-  ],
+  ]
 }
